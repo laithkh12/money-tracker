d674d3ad7b7f6000961a83abc6fd987032d1541d
laith12khater <laithkhater12@gmail.com>
2024-12-03 13:49:13 +0200

    code added

---
+++ money-tracker/api/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const Transaction = require('./models/Transaction.js');
+const mongoose  = require('mongoose');
+const app = express();
+
+// Middleware to parse JSON
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/test', (req, res) => {
+  res.json('test ok');
+});
+
+app.post('/api/transaction', async (req, res) => {
+  await mongoose.connect(process.env.MONGO_URL);
+  const {name, description, datetime, price} = req.body;
+  const transaction = await Transaction.create({name, description, datetime, price}) 
+  res.json(transaction);    
+});
+
+app.get('/api/transactions', async (req,res)=>{
+  await mongoose.connect(process.env.MONGO_URL)
+  const transactions = await Transaction.find();
+  res.json(transactions)
+})
+
+app.listen(4040, () => {
+  console.log('Server running on http://localhost:4040'); 
+});
+
